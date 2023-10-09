9cb34a83fc1349a5f3a7249d2eea9ebb5950d8da
Mazharul Islam <swopnil420@gmail.com>
2023-10-09 06:04:51 +0600

    Complete the number and text convert application

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,155 @@
-<script setup></script>
+<script>
+export default {
+	data() {
+		return {
+			numbers: {
+				decimal: 0,
+				binary: 0,
+				octal: 0,
+				hexadecimal: 0,
+			},
+			lines: {
+				text: '',
+				binary: '',
+			},
+			invalidNumber: false,
+			invalidLine: false,
+		};
+	},
+	methods: {
+		isBinary(text) {
+			for (let txt of text) {
+				if (txt !== '0' && txt !== '1') return false;
+			}
+			return true;
+		},
+
+		isOctal(text) {
+			for (let txt of text) {
+				if (txt < '0' || txt > '7') return false;
+			}
+			return true;
+		},
+		isHex(text) {
+			for (let txt of text) {
+				if (
+					(txt < '0' || txt > '9') &&
+					(txt < 'A' || txt > 'F') &&
+					(txt < 'a' || txt > 'f')
+				) {
+					return false;
+				}
+			}
+			return true;
+		},
+		resetNumbers() {
+			this.numbers = {
+				decimal: 0,
+				binary: 0,
+				octal: 0,
+				hexadecimal: 0,
+			};
+			this.invalidNumber = false;
+		},
+		textToBinary(text) {
+			const charCodeArr = [];
+			for (let i in text) {
+				charCodeArr.push(text.charCodeAt(i));
+			}
+
+			const binaryArr = charCodeArr.map((char) => {
+				return char.toString(2);
+			});
+
+			return binaryArr;
+		},
+		binaryToText(codeArray) {
+			let text = '';
+			for (let code of codeArray) {
+				const char = String.fromCharCode(parseInt(code, 2));
+				text = text.concat(char);
+			}
+			return text;
+		},
+		resetText() {
+			this.lines = {
+				text: '',
+				binary: '',
+			};
+			this.invalidLine = false;
+		},
+	},
+	watch: {
+		'numbers.decimal': function (value) {
+			this.numbers.decimal = parseInt(value) || 0;
+			this.numbers.binary = value.toString(2);
+			this.numbers.octal = value.toString(8);
+			this.numbers.hexadecimal = value.toString(16);
+		},
+
+		'numbers.binary': function (value) {
+			let decimal = parseInt(value, 2);
+
+			if (!this.isBinary(value)) {
+				this.invalidNumber = true;
+			} else {
+				this.invalidNumber = false;
+			}
+
+			this.numbers.decimal = decimal;
+			this.numbers.binary = value || 0;
+			this.numbers.octal = decimal.toString(8);
+			this.numbers.hexadecimal = decimal.toString(16);
+		},
+		'numbers.octal': function (value) {
+			let decimal = parseInt(`0${value}`, 8) || 0;
+
+			if (!this.isOctal(value)) {
+				this.invalidNumber = true;
+			} else {
+				this.invalidNumber = false;
+			}
+
+			this.numbers.decimal = decimal;
+			this.numbers.binary = decimal.toString(2) || 0;
+			this.numbers.octal = value || 0;
+			this.numbers.hexadecimal = decimal.toString(16);
+		},
+		'numbers.hexadecimal': function (value) {
+			let decimal = parseInt(`0x${value}`, 16) || 0;
+
+			if (!this.isHex(value)) {
+				this.invalidNumber = true;
+			} else {
+				this.invalidNumber = false;
+			}
+
+			this.numbers.decimal = decimal;
+			this.numbers.binary = decimal.toString(2) || 0;
+			this.numbers.octal = decimal.toString(8) || 0;
+			this.numbers.hexadecimal = value || 0;
+		},
+		'lines.text': function (value) {
+			if (value.length === 0) {
+				this.lines.binary = 0;
+			} else {
+				const codeArray = this.textToBinary(value);
+				this.lines.binary = codeArray.join(' ');
+			}
+		},
+		'lines.binary': function (value) {
+			if (value.length === 0) {
+				this.lines.text = '';
+			} else {
+				const codeArray = value.split(' ');
+				const text = this.binaryToText(codeArray);
+
+				this.lines.text = text;
+			}
+		},
+	},
+};
+</script>
 
 <template>
 	<div class="container">
@@ -13,23 +164,40 @@
 				<h2>Number System Conversion</h2>
 				<div class="box">
 					<div class="input-group">
-						<label for="decimal">Decimal</label>
-						<input type="text" value="0" aria-label="decimal" />
+						<label>Decimal</label>
+						<input
+							type="text"
+							aria-label="decimal"
+							v-model="numbers.decimal"
+						/>
 					</div>
 					<div class="input-group">
-						<label for="binary">Binary</label>
-						<input type="text" value="0" aria-label="binary" />
+						<label>Binary</label>
+						<input
+							type="text"
+							aria-label="binary"
+							v-model="numbers.binary"
+						/>
 					</div>
 					<div class="input-group">
-						<label for="octal">Octal</label>
-						<input type="text" value="0" aria-label="octal" />
+						<label>Octal</label>
+						<input
+							type="text"
+							aria-label="octal"
+							v-model="numbers.octal"
+						/>
 					</div>
 					<div class="input-group">
-						<label for="hexadecimal">Hexadecimal</label>
-						<input type="text" value="0" aria-label="hexadecimal" />
+						<label>Hexadecimal</label>
+						<input
+							type="text"
+							aria-label="hexadecimal"
+							v-model="numbers.hexadecimal"
+						/>
 					</div>
 					<div class="reset-btn">
-						<button>Reset</button>
+						<p v-if="invalidNumber">Invalid Number</p>
+						<button @click="resetNumbers">Reset</button>
 					</div>
 				</div>
 			</section>
@@ -44,6 +212,7 @@
 							id=""
 							cols="30"
 							rows="10"
+							v-model="lines.text"
 						></textarea>
 					</div>
 					<div class="input-group">
@@ -53,10 +222,12 @@
 							id=""
 							cols="30"
 							rows="10"
+							v-model="lines.binary"
 						></textarea>
 					</div>
 					<div class="reset-btn">
-						<button>Reset</button>
+						<p v-if="invalidLine">Invalid Text</p>
+						<button @click="resetText">Reset</button>
 					</div>
 				</div>
 			</section>
@@ -64,4 +235,4 @@
 	</div>
 </template>
 
-<style scoped></style>
+<!-- <style scoped></style> -->
--- src/main.js
+++ src/main.js
@@ -3,4 +3,112 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+// export default createApp(App).mount('#app');
+
+export default createApp(App, {
+	data() {
+		return {
+			numbers: {
+				decimal: 100,
+				binary: 0,
+				octal: 0,
+				hexadecimal: 0,
+			},
+		};
+	},
+	methods: {
+		isBinary(text) {
+			for (let txt of text) {
+				if (txt !== '0' && txt !== '1') return false;
+			}
+			return true;
+		},
+
+		isOctal(text) {
+			for (let txt of text) {
+				if (txt < '0' || txt > '7') return false;
+			}
+			return true;
+		},
+		isHex(text) {
+			for (let txt of text) {
+				if (
+					(txt < '0' || txt > '9') &&
+					(txt < 'A' || txt > 'F') &&
+					(txt < 'a' || txt > 'f')
+				) {
+					return false;
+				}
+			}
+			return true;
+		},
+	},
+	watch: {
+		'numbers.decimal': function (value) {
+			console.log(value);
+			this.numbers.decimal = parseInt(value) || 0;
+			this.numbers.binary = value.toString(2);
+			this.numbers.octal = value.toString(8);
+			this.numbers.hexadecimal = value.toString(16);
+		},
+
+		'numbers.binary': function (value) {
+			let decimal = parseInt(value, 2);
+
+			if (!this.isBinary(value)) {
+				this.invalidNumber = true;
+			} else {
+				this.invalidNumber = false;
+			}
+
+			this.numbers.decimal = decimal;
+			this.numbers.binary = value || 0;
+			this.numbers.octal = decimal.toString(8);
+			this.numbers.hexadecimal = decimal.toString(16);
+		},
+		'numbers.octal': function (value) {
+			let decimal = parseInt(`0${value}`, 8) || 0;
+
+			if (!this.isOctal(value)) {
+				this.invalidNumber = true;
+			} else {
+				this.invalidNumber = false;
+			}
+
+			this.numbers.decimal = decimal;
+			this.numbers.binary = decimal.toString(2) || 0;
+			this.numbers.octal = value || 0;
+			this.numbers.hexadecimal = decimal.toString(16);
+		},
+		'numbers.hexadecimal': function (value) {
+			let decimal = parseInt(`0x${value}`, 16) || 0;
+
+			if (!this.isHex(value)) {
+				this.invalidNumber = true;
+			} else {
+				this.invalidNumber = false;
+			}
+
+			this.numbers.decimal = decimal;
+			this.numbers.binary = decimal.toString(2) || 0;
+			this.numbers.octal = decimal.toString(8) || 0;
+			this.numbers.hexadecimal = value || 0;
+		},
+	},
+}).mount('#app');
+
+// const app = createApp(App, {
+// 	data() {
+// 		return {
+// 			numbers: {
+// 				decimal: 100,
+// 				binary: 0,
+// 				octal: 0,
+// 				hexadecimal: 0,
+// 			},
+// 		};
+// 	},
+// });
+// app.mount('#app');
+
+// export default app;
